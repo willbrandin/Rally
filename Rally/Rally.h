5e71665834c9b9cf1176c2e8028e07377c8e9019
willbrandin <will@tidelaundry.com>
2019-08-01 08:35:26 -0500

    Initial Commit

---
+++ Rally/Rally.h
@@ -0,0 +1,19 @@
+//
+//  Rally.h
+//  Rally
+//
+//  Created by Will Brandin on 8/1/19.
+//  Copyright © 2019 Will Brandin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Rally.
+FOUNDATION_EXPORT double RallyVersionNumber;
+
+//! Project version string for Rally.
+FOUNDATION_EXPORT const unsigned char RallyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rally/PublicHeader.h>
+
+
